1d24a25ed53d125b5f04672b96b5da9638901d46
Stefan Engvall <steen919@gmail.com>
2012-12-17 22:17:50 +0100

    Add variables for TextViews and Buttons

--- PersonDemo/src/se/jbwebb/sengvall/persondemo/FormActivity.java
+++ PersonDemo/src/se/jbwebb/sengvall/persondemo/FormActivity.java
@@ -1,14 +1,29 @@
 package se.jbwebb.sengvall.persondemo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class FormActivity extends Activity {
+	
+	TextView tvName;
+	TextView tvAddress;
+	TextView tvZipCity;
+	TextView tvMobile;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_form);
+		
+		tvName = (TextView) findViewById(R.id.editText1);
+		tvAddress = (TextView) findViewById(R.id.editText2);
+		tvZipCity = (TextView) findViewById(R.id.editText3);
+		tvMobile = (TextView) findViewById(R.id.editText4);
+		
+		Button bSave = (Button) findViewById(R.id.button1);
+		
 	}
 
 	
